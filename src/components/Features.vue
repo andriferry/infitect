b469510c8ee1ee1fdc62018fa36c150b923622d4
andriferry <ferryandri35@gmail.com>
2022-12-07 18:56:30 +0700

    Update image features CEO

--- src/components/Features.vue
+++ src/components/Features.vue
@@ -64,7 +64,7 @@ const client = ref([
               <span class="text-secondarycolor">Founder and CTO Infitech</span>
             </p>
           </div>
-          <img class="w-[508px] h-[551px] rounded-2xl" src="https://images.unsplash.com/photo-1591084728795-1149f32d9866?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80" alt="">
+          <img class="h-[551px] rounded-2xl" src="https://images.unsplash.com/photo-1591084728795-1149f32d9866?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80" alt="">
         </div>
       </div>
     </div>
