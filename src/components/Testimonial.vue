2b3becec4974a28f44dfe907c478d9e485a2c32e
andriferry <ferryandri35@gmail.com>
2022-12-26 08:47:38 +0700

    Update Testimonial

--- src/components/Testimonial.vue
+++ src/components/Testimonial.vue
@@ -2,6 +2,12 @@
 import { ref } from 'vue';
 import {Perspective} from '@egjs/flicking-plugins';
 
+
+const options = ref({
+  renderOnlyVisible: true,
+  circular: true,
+  defaultIndex: 2,
+})
 const testimonials = ref(
   [
     {
@@ -10,23 +16,23 @@ const testimonials = ref(
       imageUrl: "bg-[url('https://images.unsplash.com/photo-1560250097-0b93528c311a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80')]"
     },
 
-    // {
-    //   name: 'Thomas S',
-    //   companyName: 'XYZ Ltd',
-    //   imageUrl: "bg-[url('https://images.unsplash.com/photo-1519085360753-af0119f7cbe7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80')]"
-    // },
+    {
+      name: 'Thomas S',
+      companyName: 'XYZ Ltd',
+      imageUrl: "bg-[url('https://images.unsplash.com/photo-1519085360753-af0119f7cbe7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80')]"
+    },
 
 
-    // {
-    //   name: 'Ken ',
-    //   companyName: 'Canvas Ltd',
-    //   imageUrl: "bg-[url('https://images.unsplash.com/photo-1562788869-4ed32648eb72?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1772&q=80')]"
-    // },
-    // {
-    //   name: 'Tom A. ',
-    //   companyName: 'Pif Ltd',
-    //   imageUrl: "bg-[url('https://images.unsplash.com/photo-1600878459138-e1123b37cb30?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjR8fG1hbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60')]"
-    // }
+    {
+      name: 'Ken ',
+      companyName: 'Canvas Ltd',
+      imageUrl: "bg-[url('https://images.unsplash.com/photo-1562788869-4ed32648eb72?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1772&q=80')]"
+    },
+    {
+      name: 'Tom A. ',
+      companyName: 'Pif Ltd',
+      imageUrl: "bg-[url('https://images.unsplash.com/photo-1600878459138-e1123b37cb30?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjR8fG1hbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60')]"
+    }
   ]
 );
 
@@ -45,9 +51,8 @@ const testimonials = ref(
         </p>
       </div>
 
-      <!-- grid mb-20 grid-flow-col auto-cols-auto gap-4  -->
 
-      <div class="grid grid-flow-col auto-cols-auto mt-10 gap-6 py-5">
+      <div class="lg:grid hidden  grid-flow-col auto-cols-auto mt-10 gap-6 py-5">
         <div v-for="(data, index) in testimonials" :key="index" class="flex flex-col p-2 gap-4">
           <p class="text-base font-medium leading-7">“Infinitech is the best team for this year! I am very appreciated with their works, especially when they helped me to grow up our business.”</p>
           <div class="flex gap-5 items-center">
@@ -60,24 +65,33 @@ const testimonials = ref(
         </div>
       </div>
 
+       <div class="flex lg:hidden justify-center">
+          <div class="w-[1388px]">
+            <!-- <Flicking :options="options" class="mt-10 py-3">
+              <div v-for="(data, index) in testimonials" :key="index" class="flex flex-col p-2 gap-4">
+                <p class="text-base font-medium leading-7">“Infinitech is the best team for this year! I am very appreciated with their works, especially when they helped me to grow up our business.”</p>
+                <div class="flex gap-5 items-center">
+                  <div class="w-16 h-16 bg-cover rounded-full" :class="data.imageUrl"></div>
+                  <div class="flex gap-2 justify-start flex-col">
+                    <p class="text-base font-semibold">{{data.name}}</p>
+                    <p class="text-xs text-[#445160E0]/75 font-medium">CTO at {{data.companyName}}</p>
+                  </div>
+                </div>
+              </div>
+            </Flicking> -->
 
-      <!-- <div class="flex h-[500px] justify-center">
-        <div class="">
-          <Flicking :options="{renderOnlyVisible: true,  circular: true, defaultIndex: 2,}" class="mt-10 py-3">
-            <div v-for="(data, index) in testimonials" :key="index" class="flex flex-col p-2 gap-4">
-          <p class="text-base font-medium leading-7">“Infinitech is the best team for this year! I am very appreciated with their works, especially when they helped me to grow up our business.”</p>
-          <div class="flex gap-5 items-center">
-            <div class="w-16 h-16 bg-cover rounded-full" :class="data.imageUrl"></div>
-            <div class="flex gap-2 justify-start flex-col">
-              <p class="text-base font-semibold">{{data.name}}</p>
-              <p class="text-xs text-[#445160E0]/75 font-medium">CTO at {{data.companyName}}</p>
-            </div>
+             <div v-for="(data, index) in testimonials" :key="index" class="flex mt-4 flex-col p-2 gap-4">
+                <p class="text-base font-medium leading-7">“Infinitech is the best team for this year! I am very appreciated with their works, especially when they helped me to grow up our business.”</p>
+                <div class="flex gap-5 items-center">
+                  <div class="w-16 h-16 bg-cover rounded-full" :class="data.imageUrl"></div>
+                  <div class="flex gap-2 justify-start flex-col">
+                    <p class="text-base font-semibold">{{data.name}}</p>
+                    <p class="text-xs text-[#445160E0]/75 font-medium">CTO at {{data.companyName}}</p>
+                  </div>
+                </div>
+              </div>
           </div>
         </div>
-          </Flicking>
-
-        </div>
-      </div> -->
     </div>
   </div>  
 </template>
